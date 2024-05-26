978389c3970eab315ba96e9fa257727633a3c7b7
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-26 15:59:06 -0700

    Finished mobile nav! :)

--- src/app/components/header/MobileHeader.tsx
+++ src/app/components/header/MobileHeader.tsx
@@ -6,7 +6,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "~/components/ui/popover";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { HeaderNavItem, type HeaderNavItemSubProps } from "./HeaderNavItem";
 
 export function MobileHeader() {
   return (
@@ -26,6 +27,9 @@ export function MobileHeader() {
 
 function MobilePopoverNav() {
   const [navOpen, setNavOpen] = useState(false);
+  const closeNav = useCallback(() => {
+    setNavOpen(false);
+  }, [setNavOpen]);
 
   return (
     <Popover open={navOpen} onOpenChange={setNavOpen}>
@@ -34,9 +38,32 @@ function MobilePopoverNav() {
           className={`size-10 rounded-md border-[1px] border-[#B3895D] bg-[#D6AB7D] stroke-2 p-1 text-[#482919] active:border-opacity-30 active:bg-opacity-30 active:text-opacity-80 ${navOpen ? "border-opacity-25 bg-opacity-30 text-opacity-80" : "border-opacity-0 bg-opacity-0 text-opacity-60"}`}
         />
       </PopoverTrigger>
-      <PopoverContent side="bottom" className="mr-2">
-        <p>test popover content</p>
+      <PopoverContent
+        side="bottom"
+        className="mr-2 w-fit p-0"
+        onClick={closeNav}
+      >
+        <nav className="font-title flex flex-col items-stretch gap-0 text-right text-xl tracking-wide">
+          <MobileHeaderNavItem href="/">Home</MobileHeaderNavItem>
+          <MobileHeaderNavItem href="/about">About Us</MobileHeaderNavItem>
+          <MobileHeaderNavItem href="/policies">Policies</MobileHeaderNavItem>
+          <MobileHeaderNavItem href="/contact">Contact</MobileHeaderNavItem>
+          <MobileHeaderNavItem href="/more">More Events</MobileHeaderNavItem>
+        </nav>
       </PopoverContent>
     </Popover>
+  );
+}
+
+function MobileHeaderNavItem({
+  activeClassStr = "bg-[#7A7265] bg-opacity-20",
+  ...props
+}: HeaderNavItemSubProps) {
+  return (
+    <HeaderNavItem
+      activeClassStr={activeClassStr}
+      className="border-b-[1px] border-[#B3895D] border-opacity-50 px-6 py-3 last:border-b-0"
+      {...props}
+    />
   );
 }
