7bc8278d9d1caa72bbadffed189a38ce3c1f45ca
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-25 23:16:15 -0700

    Start of mobile header building. So far so good, need to build hamburger menu dropdown

--- src/app/components/header/mobileHeader/MobileHeader.tsx
+++ src/app/components/header/mobileHeader/MobileHeader.tsx
@@ -1,7 +1,25 @@
+import { MenuIcon } from "lucide-react";
+import ContraLogo from "../ContraLogo";
+import styles from "./MobileHeader.module.css";
+
 export default function MobileHeader() {
   return (
-    <header className={`desktop:hidden block`}>
-      <nav></nav>
+    <header
+      className={`desktop:hidden relative flex flex-col ${styles.header}`}
+    >
+      <MenuIcon strokeWidth={2} size={36} className="absolute right-2 top-2" />
+      <div className="flex flex-col items-center gap-2 p-4">
+        <div className="grid w-full grid-cols-[1fr_auto_1fr] items-center">
+          <div className="-mr-2 border-y-2 border-black py-1">
+            <div className="border-y-4 border-black py-2"></div>
+          </div>
+          <ContraLogo height={128} />
+          <div className="-ml-2 border-y-2 border-black py-1">
+            <div className="border-y-4 border-black py-2"></div>
+          </div>
+        </div>
+        <h2 className="text-4xl md:text-5xl">Santa Cruz Contra</h2>
+      </div>
     </header>
   );
 }
