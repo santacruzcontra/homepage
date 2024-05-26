be789d169b7d96cb6cc7c8d830852ed79e1f8c23
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-25 23:15:43 -0700

    Remove extra title, we're sticking it in the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ export default function HomePage() {
   return (
     <main>
       <div>
-        <h1>Santa Cruz Contra</h1>
-        <p className="text-4xl font-semibold">Coming soon!</p>
+        <p className="text-4xl font-semibold">Home</p>
       </div>
     </main>
   );
