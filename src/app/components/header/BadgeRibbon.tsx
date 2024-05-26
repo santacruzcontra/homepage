8dc1c58bee188e656c05c3cff54682f071dee5db
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-26 11:00:04 -0700

    Move BadgeRibbon into its own component. Generalize it and support children so that we can use it on mobile and desktop with no issue

---
+++ src/app/components/header/BadgeRibbon.tsx
@@ -0,0 +1,21 @@
+/**
+ * Badge ribbon component, styled with double borders on top
+ * and bottom with different sizes. Supports children so that
+ * it can be used on desktop and mobile headers.
+ */
+export default function BadgeRibbon({
+  left,
+  children,
+}: React.PropsWithChildren<
+  { left: true; right?: never } | { right: true; left?: never }
+>) {
+  return (
+    <div
+      className={`relative flex w-full flex-grow flex-row ${left ? "left-4" : "right-4"} border-y-2 border-black py-1`}
+    >
+      <div className="flex flex-grow flex-row items-center justify-evenly gap-8 border-y-4 border-black p-2">
+        {children}
+      </div>
+    </div>
+  );
+}
