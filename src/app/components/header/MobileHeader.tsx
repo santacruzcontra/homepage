6b85b2f0b921376970c9b683c16683e3d1ed7c03
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-26 11:15:44 -0700

    Add dummy popover for navigation menu

--- src/app/components/header/MobileHeader.tsx
+++ src/app/components/header/MobileHeader.tsx
@@ -1,14 +1,26 @@
 import { MenuIcon } from "lucide-react";
 import { BadgeRibbon } from "./BadgeRibbon";
 import { ContraLogo } from "./ContraLogo";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 
 export function MobileHeader() {
   return (
     <header className="relative flex flex-col desktop:hidden">
-      <MenuIcon
-        size={36}
-        className="absolute right-2 top-2 stroke-2 text-stone-800 text-opacity-60 hover:text-opacity-100 active:text-opacity-100"
-      />
+      <Popover>
+        <PopoverTrigger className="absolute right-2 top-2">
+          <MenuIcon
+            size={36}
+            className="stroke-2 text-stone-800 text-opacity-60 hover:text-opacity-100 active:text-opacity-100"
+          />
+        </PopoverTrigger>
+        <PopoverContent side="bottom">
+          <p>test popover content</p>
+        </PopoverContent>
+      </Popover>
       <div className="flex flex-col items-center gap-2 px-2 py-4">
         <div className="grid w-full grid-cols-[1fr_auto_1fr] items-center">
           <BadgeRibbon left />
