37b1db126e6b11ed5828f3371067286a8acf36e7
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-25 17:18:10 -0700

    Add in pages

---
+++ src/app/more/page.tsx
@@ -0,0 +1,9 @@
+export default function MoreVenuesPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-stretch justify-start text-black">
+      <div>
+        <h1>More Contra Venues</h1>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#6d0202] to-[#2c1515] text-white">
+    <main className="flex min-h-screen flex-col items-stretch justify-start text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <h1 className="text-5xl font-bold tracking-tight sm:text-[5rem]">
           Santa Cruz Contra
         </h1>
-        <p className="text-4xl font-bold">Coming soon!</p>
+        <p className="text-4xl font-semibold">Coming soon!</p>
       </div>
     </main>
   );
