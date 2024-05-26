ca4e340d9c8bae466695791ac331424a3df2eb6c
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-26 11:43:29 -0700

    Fix page height issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${montserrat.className} ${bebasNeue.variable}`}>
-      <body className="bg-gradient-to-b from-[#f8f0e5] to-[#e2c7aa]  text-stone-950">
+      <body className="min-h-dvh bg-gradient-to-b from-[#f8f0e5]  to-[#e2c7aa] text-stone-950">
         <Header />
         {children}
       </body>
