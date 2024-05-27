2d1703075be4cb3b64c5a12fc1a4517f762c8d9c
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-27 10:53:01 -0700

    Split the contact form cards into their own functions to make refactoring of the top-level component easier. Reorder the cards so the newsletter is first.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,40 +17,52 @@ export default function ContactPage() {
     <>
       <PageTitle>Contact</PageTitle>
       <div className="m-auto grid max-w-md grid-cols-1 items-stretch justify-start gap-4 p-4 desktop:mt-4 desktop:max-w-4xl desktop:grid-cols-2">
-        <Card className="flex flex-col justify-between">
-          <CardHeader className="gap-2">
-            <CardTitle>Message our volunteers</CardTitle>
-            <CardDescription className="text-stone-700">
-              Send us a message if you&apos;d like to discuss volunteering or
-              would like to offer up a suggestion.
-            </CardDescription>
-            <CardDescription className="text-stone-700">
-              The following button will open a form where you can easily send us
-              an email. Our volunteers will respond as soon as they can!
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Link href={env.GOOGLE_CONTACT_FORM_URL} target="_blank">
-              <Button className="w-full">
-                Send a message&nbsp;&nbsp;&nbsp;
-                <ExternalLinkIcon />
-              </Button>
-            </Link>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Get the newsletter</CardTitle>
-            <CardDescription className="max-w-sm text-stone-700">
-              Enter your email to subscribe to our monthly newsletter and hear
-              about upcoming dances.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <SubscribeEmailForm />
-          </CardContent>
-        </Card>
+        <SubscribeNewsletterCard />
+        <FormLinkCard />
       </div>
     </>
+  );
+}
+
+function FormLinkCard() {
+  return (
+    <Card className="flex flex-col justify-between">
+      <CardHeader className="gap-2">
+        <CardTitle>Message our volunteers</CardTitle>
+        <CardDescription className="text-stone-700">
+          Send us a message if you&apos;d like to discuss volunteering or would
+          like to offer up a suggestion.
+        </CardDescription>
+        <CardDescription className="text-stone-700">
+          The following button will open a form where you can easily send us an
+          email. Our volunteers will respond as soon as they can!
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Link href={env.GOOGLE_CONTACT_FORM_URL} target="_blank">
+          <Button className="w-full">
+            Send a message&nbsp;&nbsp;&nbsp;
+            <ExternalLinkIcon />
+          </Button>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+}
+
+function SubscribeNewsletterCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Get the newsletter</CardTitle>
+        <CardDescription className="max-w-sm text-stone-700">
+          Enter your email to subscribe to our monthly newsletter and hear about
+          upcoming dances.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <SubscribeEmailForm />
+      </CardContent>
+    </Card>
   );
 }
