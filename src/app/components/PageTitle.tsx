023740e8e03c774d2c64890337e61cbca3ebbc8b
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-27 08:35:07 -0700

    New title media queries

--- src/app/components/PageTitle.tsx
+++ src/app/components/PageTitle.tsx
@@ -5,7 +5,7 @@ export function PageTitle({
 }: React.PropsWithChildren<React.HTMLProps<HTMLHeadingElement>>) {
   return (
     <h1
-      className={`${className ? className + " " : ""} font-title w-full text-center text-4xl sm:text-5xl md:text-6xl desktop:text-7xl xl:text-8xl`}
+      className={`${className ? className + " " : ""} font-title w-full text-center text-4xl min-[525px]:text-5xl sm:text-6xl desktop:text-7xl xl:text-8xl`}
       {...props}
     >
       {children}
