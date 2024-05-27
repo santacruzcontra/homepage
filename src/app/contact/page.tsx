08c36e076d48f863d85a3946d181fe1eae8dbc3d
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-27 10:44:12 -0700

    Contact page is done I think! Now includes a link to the Google form for sending messages, as well as the email entry form to subscribe to our newsletter

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,56 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import { PageTitle } from "../components/PageTitle";
 import { SubscribeEmailForm } from "./SubscribeEmailForm";
+import { Button } from "~/components/ui/button";
+import Link from "next/link";
+import { env } from "~/env";
+import { ExternalLinkIcon } from "lucide-react";
 
 export default function ContactPage() {
   return (
     <>
       <PageTitle>Contact</PageTitle>
-      <SubscribeEmailForm />
+      <div className="m-auto grid max-w-md grid-cols-1 items-stretch justify-start gap-4 p-4 desktop:mt-4 desktop:max-w-4xl desktop:grid-cols-2">
+        <Card className="flex flex-col justify-between">
+          <CardHeader className="gap-2">
+            <CardTitle>Message our volunteers</CardTitle>
+            <CardDescription className="text-stone-700">
+              Send us a message if you&apos;d like to discuss volunteering or
+              would like to offer up a suggestion.
+            </CardDescription>
+            <CardDescription className="text-stone-700">
+              The following button will open a form where you can easily send us
+              an email. Our volunteers will respond as soon as they can!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Link href={env.GOOGLE_CONTACT_FORM_URL} target="_blank">
+              <Button className="w-full">
+                Send a message&nbsp;&nbsp;&nbsp;
+                <ExternalLinkIcon />
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Get the newsletter</CardTitle>
+            <CardDescription className="max-w-sm text-stone-700">
+              Enter your email to subscribe to our monthly newsletter and hear
+              about upcoming dances.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <SubscribeEmailForm />
+          </CardContent>
+        </Card>
+      </div>
     </>
   );
 }
