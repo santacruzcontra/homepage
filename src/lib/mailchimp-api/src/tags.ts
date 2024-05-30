cee021121fd8614731adc9ffb5e4c4b95c10b484
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-30 01:13:52 -0700

    Better typing

--- src/lib/mailchimp-api/src/tags.ts
+++ src/lib/mailchimp-api/src/tags.ts
@@ -1,15 +1,21 @@
 import { env } from "~/env";
 import { _BaseMailchimpAPI } from "./_BaseMailchimpAPI";
+import { type MailchimpErrorRes } from "./helpers";
 
 export const WEBSITE = "Website";
-export function add(subscriberHash: string, tags: string | string[]) {
+export const DISCC = "DISCC";
+
+export type AddTagSuccessRes = Record<string, never>;
+
+export function add(
+  subscriberHash: string,
+  tags: string | string[],
+): Promise<AddTagSuccessRes | MailchimpErrorRes> {
   const tagArr = typeof tags === "string" ? [tags] : tags;
   const mappedTags = tagArr.map((tag) => ({ name: tag, status: "active" }));
 
   return _BaseMailchimpAPI._Post(
     `/lists/${env.MAILCHIMP_API_AUDIENCE_ID}/members/${subscriberHash}/tags`,
-    {
-      tags: mappedTags,
-    },
+    { tags: mappedTags },
   );
 }
