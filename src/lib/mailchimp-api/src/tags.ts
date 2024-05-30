332d511a083b21722c18805ce944e22e36503107
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-30 10:15:00 -0700

    Split out the tag API call to allow add/remove/update actions on tags. Split code to be more reusable.

--- src/lib/mailchimp-api/src/tags.ts
+++ src/lib/mailchimp-api/src/tags.ts
@@ -5,17 +5,33 @@ import { type MailchimpErrorRes } from "./helpers";
 export const WEBSITE = "Website";
 export const DISCC = "DISCC";
 
-export type AddTagSuccessRes = Record<string, never>;
+export type UpdateTagSuccessRes = Record<string, never>;
+export type TagSpec = { name: string; status: "active" | "inactive" };
 
-export function add(
+export function update(
   subscriberHash: string,
-  tags: string | string[],
-): Promise<AddTagSuccessRes | MailchimpErrorRes> {
-  const tagArr = typeof tags === "string" ? [tags] : tags;
-  const mappedTags = tagArr.map((tag) => ({ name: tag, status: "active" }));
+  tagChanges: TagSpec | TagSpec[],
+): Promise<UpdateTagSuccessRes | MailchimpErrorRes> {
+  const tagArr = Array.isArray(tagChanges) ? tagChanges : [tagChanges];
 
   return _BaseMailchimpAPI._Post(
     `/lists/${env.MAILCHIMP_API_AUDIENCE_ID}/members/${subscriberHash}/tags`,
-    { tags: mappedTags },
+    { tags: tagArr },
   );
+}
+
+function formatTagStrings(tags: string | string[], active: boolean) {
+  const tagArr = typeof tags === "string" ? [tags] : tags;
+  return tagArr.map<TagSpec>((tag) => ({
+    name: tag,
+    status: active ? "active" : "inactive",
+  }));
+}
+
+export function add(subscriberHash: string, tags: string | string[]) {
+  return update(subscriberHash, formatTagStrings(tags, true));
+}
+
+export function remove(subscriberHash: string, tags: string | string[]) {
+  return update(subscriberHash, formatTagStrings(tags, false));
 }
