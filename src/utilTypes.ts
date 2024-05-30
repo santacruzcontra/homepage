244b8d27ed17ffb6be2dd88db214b9acac8f51fc
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-30 11:36:23 -0700

    New "RequiredKeys" utility type

--- src/utilTypes.ts
+++ src/utilTypes.ts
@@ -1,4 +1,9 @@
 export type OptionalKeys<
   BaseType extends Record<string, unknown>,
-  Keys extends string,
+  Keys extends keyof BaseType,
 > = Omit<BaseType, Keys> & Partial<Pick<BaseType, Keys>>;
+
+export type RequiredKeys<
+  BaseType extends Record<string, unknown>,
+  Keys extends keyof BaseType,
+> = Omit<BaseType, Keys> & Required<Pick<BaseType, Keys>>;
