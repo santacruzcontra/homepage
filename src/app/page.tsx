02c288ea614eefc596ef36179fd7d4d5d39a494c
bramirez96 <bran.ramirez.don@gmail.com>
2024-07-26 12:22:18 -0700

    Update homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,129 +83,125 @@ export default function HomePage() {
         </div>
       </section>
       <Separator/>
-      <section className="m-auto flex w-full flex-row flex-wrap items-start justify-center gap-4 p-4">
-        <Card className="w-full max-w-2xl">
-          <CardHeader>
-            <CardTitle>Common Questions</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Separator />
-            <Accordion type="single" collapsible>
-              <AccordionItem value="partner">
-                <AccordionTrigger>Do I need a partner?</AccordionTrigger>
-                <AccordionContent>
-                  <p>
-                    No! Many of our dancers come alone, and those who come with
-                    others will usually switch partners between dances. Come
-                    alone or bring some friends!
-                  </p>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="whatToWear">
-                <AccordionTrigger>What should I bring?</AccordionTrigger>
-                <AccordionContent>
-                  <ul className="list-disc pl-4">
-                    <li>
-                      Comfortable, non-marking shoes to protect your feet and
-                      our floors.
-                    </li>
-                    <li>
-                      Loose-fitting and light-weight clothing to keep you cool.
-                    </li>
-                    <li>A water bottle is recommended.</li>
-                    <li>
-                      Avoid strong fragrances as many of our dancers have
-                      sensitivities.
-                    </li>
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="who">
-                <AccordionTrigger>Who hosts the events?</AccordionTrigger>
-                <AccordionContent>
-                  <p>
-                    Our events are hosted by Dance Inclusion of Santa Cruz
-                    County, a non-profit organization made up of a number of
-                    dedicated volunteers and community members from all over.
-                  </p>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="cost">
-                <AccordionTrigger>How much does it cost?</AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-2">
-                  <p>
-                    Admission is sliding scale: $15-$30 or pay what you can
-                    (Cash or Check only).
-                  </p>
-                  <p>
-                    That said, we&apos;re a non-profit, and we want you to come
-                    dance more than we want your money! No one will be turned
-                    away for lack of funds.
-                  </p>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="contribute">
-                <AccordionTrigger>How can I contribute?</AccordionTrigger>
-                <AccordionContent>
-                  <p>
-                    You can contribute by volunteering to help us with our
-                    events, or by donating to our non-profit organization. Speak
-                    to one of our managers at a dance, reach out to one of the
-                    admins on our{" "}
-                    <LinkToExternal
-                      href={env.SC_CONTRA_FACEBOOK_LINK}
-                      icon={false}
-                    >
-                      Facebook group
-                    </LinkToExternal>
-                    , or send us a message through our{" "}
-                    <LinkToExternal
-                      href={env.GOOGLE_CONTACT_FORM_URL}
-                      icon={false}
-                    >
-                      contact form
-                    </LinkToExternal>
-                    .
-                  </p>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="learnMore">
-                <AccordionTrigger>Where can I learn more?</AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-2">
-                  <p>
-                    There are many resources and videos across the web where you
-                    can learn more about Contra Dance. Here are some video links
-                    to get you started:
-                  </p>
-                  <ul className="flex list-disc flex-col gap-2 p-2 pl-4">
-                    <li>
-                      <LinkToExternal href="https://youtu.be/T9_Qbnpplgc">
-                        Beginner? Just have fun!
-                      </LinkToExternal>
-                    </li>
-                    <li>
-                      <LinkToExternal href="https://youtu.be/wsCOSdx1E9E">
-                        Contra Dance Camp 2023
-                      </LinkToExternal>
-                    </li>
-                    <li>
-                      <LinkToExternal href="https://youtu.be/jcYOIXIMfJQ">
-                        Experienced Contra Dancers
-                      </LinkToExternal>
-                    </li>
-                    <li>
-                      <LinkToExternal href="https://youtu.be/M_-qnZ0_fbE">
-                        Queer Contra in SF bay
-                      </LinkToExternal>
-                    </li>
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </CardContent>
-        </Card>
+      <section className="flex flex-col items-stretch justify-center p-8">
+        <div className="flex flex-col items-stretch gap-4">
+          <h2 className="font-title text-4xl italic">Common Questions</h2>
+          <Separator />
+        </div>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="partner">
+            <AccordionTrigger>Do I need a partner?</AccordionTrigger>
+            <AccordionContent>
+              <p>
+                No! Many of our dancers come alone, and those who come with
+                others will usually switch partners between dances. Come
+                alone or bring some friends!
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="whatToWear">
+            <AccordionTrigger>What should I bring?</AccordionTrigger>
+            <AccordionContent>
+              <ul className="list-disc pl-4">
+                <li>
+                  Comfortable, non-marking shoes to protect your feet and
+                  our floors.
+                </li>
+                <li>
+                  Loose-fitting and light-weight clothing to keep you cool.
+                </li>
+                <li>A water bottle is recommended.</li>
+                <li>
+                  Avoid strong fragrances as many of our dancers have
+                  sensitivities.
+                </li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="who">
+            <AccordionTrigger>Who hosts the events?</AccordionTrigger>
+            <AccordionContent>
+              <p>
+                Our events are hosted by Dance Inclusion of Santa Cruz
+                County, a non-profit organization made up of a number of
+                dedicated volunteers and community members from all over.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="cost">
+            <AccordionTrigger>How much does it cost?</AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-2">
+              <p>
+                Admission is sliding scale: $15-$30 or pay what you can
+                (Cash or Check only).
+              </p>
+              <p>
+                That said, we&apos;re a non-profit, and we want you to come
+                dance more than we want your money! No one will be turned
+                away for lack of funds.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="contribute">
+            <AccordionTrigger>How can I contribute?</AccordionTrigger>
+            <AccordionContent>
+              <p>
+                You can contribute by volunteering to help us with our
+                events, or by donating to our non-profit organization. Speak
+                to one of our managers at a dance, reach out to one of the
+                admins on our{" "}
+                <LinkToExternal
+                    href={env.SC_CONTRA_FACEBOOK_LINK}
+                    icon={false}
+                >
+                  Facebook group
+                </LinkToExternal>
+                , or send us a message through our{" "}
+                <LinkToExternal
+                    href={env.GOOGLE_CONTACT_FORM_URL}
+                    icon={false}
+                >
+                  contact form
+                </LinkToExternal>
+                .
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="learnMore">
+            <AccordionTrigger>Where can I learn more?</AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-2">
+              <p>
+                There are many resources and videos across the web where you
+                can learn more about Contra Dance. Here are some video links
+                to get you started:
+              </p>
+              <ul className="flex list-disc flex-col gap-2 p-2 pl-4">
+                <li>
+                  <LinkToExternal href="https://youtu.be/T9_Qbnpplgc">
+                    Beginner? Just have fun!
+                  </LinkToExternal>
+                </li>
+                <li>
+                  <LinkToExternal href="https://youtu.be/wsCOSdx1E9E">
+                    Contra Dance Camp 2023
+                  </LinkToExternal>
+                </li>
+                <li>
+                  <LinkToExternal href="https://youtu.be/jcYOIXIMfJQ">
+                    Experienced Contra Dancers
+                  </LinkToExternal>
+                </li>
+                <li>
+                  <LinkToExternal href="https://youtu.be/M_-qnZ0_fbE">
+                    Queer Contra in SF bay
+                  </LinkToExternal>
+                </li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </section>
-      <section className="m-auto w-full max-w-4xl p-4"></section>
+      <Separator className="mt-8" />
     </>
   );
 }
