95fe5d4425d7dbc75aa0802784fcda0f2206f9a6
bramirez96 <bran.ramirez.don@gmail.com>
2024-07-26 12:44:53 -0700

    Convert to no longer be inside of cards and stuff

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,23 +1,16 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { PageTitle } from "../components/PageTitle";
 import { SubscribeEmailForm } from "./SubscribeEmailForm";
 import { Button } from "~/components/ui/button";
 import Link from "next/link";
 import { env } from "~/env";
 import { ExternalLinkIcon } from "lucide-react";
+import {Separator} from "~/components/ui/separator";
 
 export default function ContactPage() {
   return (
     <>
-      <PageTitle>Contact</PageTitle>
-      <div className="m-auto grid max-w-md grid-cols-1 items-stretch justify-start gap-4 p-4 lg:mt-6 lg:gap-8 lg:p-8 desktop:mt-4 desktop:max-w-4xl desktop:grid-cols-2">
+      <div className="grid items-stretch gap-4 grid-cols-[1fr_1px_1fr] py-8">
         <SubscribeNewsletterCard />
+        <Separator orientation="vertical" className="h-auto" />
         <FormLinkCard />
       </div>
     </>
@@ -26,43 +19,39 @@ export default function ContactPage() {
 
 function FormLinkCard() {
   return (
-    <Card className="flex flex-col justify-between">
-      <CardHeader className="gap-2">
-        <CardTitle>Message our volunteers</CardTitle>
-        <CardDescription>
-          Send us a message if you&apos;d like to sponsor a dance, volunteer, or
-          offer up a suggestion.
-        </CardDescription>
-        <CardDescription>
-          The following button will open a form where you can easily send us an
-          email. Our volunteers will respond as soon as they can!
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Link href={env.GOOGLE_CONTACT_FORM_URL} target="_blank">
-          <Button className="w-full">
-            Send message&nbsp;&nbsp;&nbsp;
-            <ExternalLinkIcon />
-          </Button>
-        </Link>
-      </CardContent>
-    </Card>
+      <div className="flex flex-col items-stretch justify-between">
+          <div className="flex flex-col items-stretch gap-4">
+              <h2 className="font-title italic text-3xl">Message our volunteers</h2>
+              <p>
+                  Send us a message if you&apos;d like to sponsor a dance, volunteer, or
+                  offer up a suggestion.
+              </p>
+              <p>
+                  The following button will open a form where you can easily send us an
+                  email. Our volunteers will respond as soon as they can!
+              </p>
+          </div>
+          <Link href={env.GOOGLE_CONTACT_FORM_URL} target="_blank">
+              <Button className="w-full h-12 text-base">
+                  Send message&nbsp;&nbsp;&nbsp;
+                  <ExternalLinkIcon />
+              </Button>
+          </Link>
+      </div>
   );
 }
 
 function SubscribeNewsletterCard() {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Get the newsletter</CardTitle>
-        <CardDescription>
-          Enter your email to subscribe to our monthly newsletter and hear about
-          upcoming events.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <SubscribeEmailForm />
-      </CardContent>
-    </Card>
-  );
+    return (
+        <div className="flex flex-col items-stretch justify-between">
+            <div className="flex flex-col items-stretch gap-4">
+                <h2 className="font-title italic text-3xl">Get the newsletter</h2>
+                <p>
+                    Enter your email to subscribe to our monthly newsletter and hear about
+                    upcoming events.
+                </p>
+                <SubscribeEmailForm/>
+            </div>
+        </div>
+    );
 }
