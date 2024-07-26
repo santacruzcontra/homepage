2a2f01db0d4a288f214354757094a48bf4a549ad
bramirez96 <bran.ramirez.don@gmail.com>
2024-07-26 12:02:46 -0700

    Make this h1

--- src/app/components/header/DesktopHeader.tsx
+++ src/app/components/header/DesktopHeader.tsx
@@ -3,7 +3,7 @@ import {HeaderNavItem, type HeaderNavItemSubProps} from "./HeaderNavItem";
 export function DesktopHeader() {
 	return (
 		<header className="flex flex-col gap-2 w-full">
-            <h2 className="w-full text-center font-title text-4xl">Santa Cruz Contra Dance</h2>
+            <h1 className="w-full text-center font-title text-4xl">Santa Cruz Contra Dance</h1>
 			<nav className="w-full flex flex-row gap-8 text-xl justify-center h-10 items-center">
 				<DesktopHeaderNavItem href="/">Home</DesktopHeaderNavItem>
 				<DesktopHeaderNavItem href="/policies">Policies</DesktopHeaderNavItem>
