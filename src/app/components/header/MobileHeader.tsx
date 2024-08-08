9e41df0e40be50456e2160484df53e980052fa37
bramirez96 <bran.ramirez.don@gmail.com>
2024-08-07 21:54:11 -0700

    Change the scaling of the header

--- src/app/components/header/MobileHeader.tsx
+++ src/app/components/header/MobileHeader.tsx
@@ -15,7 +15,7 @@ export function MobileHeader() {
         <MobilePopoverNav />
         <div className="flex flex-col items-start gap-2 p-4">
           <Link href="/">
-            <h1 className="font-title text-lg min-[500px]:text-2xl">
+            <h1 className="font-title text-xl min-[400px]:text-2xl">
               Santa Cruz Contra Dance
             </h1>
           </Link>
@@ -33,7 +33,7 @@ function MobilePopoverNav() {
 
   return (
     <Popover open={navOpen} onOpenChange={setNavOpen}>
-      <PopoverTrigger className="absolute right-2.5 top-2.5 min-[500px]:right-3 min-[500px]:top-3">
+      <PopoverTrigger className="absolute right-2.5 top-2.5 min-[400px]:right-3 min-[400px]:top-3">
         <MenuIcon
           className={`size-10 rounded-md border-[1px] border-[#bf490e] bg-[#ffbf69] stroke-2 p-1 text-[#bf490e] active:border-opacity-30 active:bg-opacity-30 active:text-opacity-60 min-[650px]:size-12 min-[650px]:p-2 md:size-14 ${navOpen ? "border-opacity-25 bg-opacity-30 text-opacity-60" : "border-opacity-0 bg-opacity-0 text-opacity-40"}`}
         />
