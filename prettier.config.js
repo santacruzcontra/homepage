ff116099eed2d47d18d9e5c1f6d1bc63d57bc532
bramirez96 <bran.ramirez.don@gmail.com>
2024-08-07 21:44:25 -0700

    Project config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 4,
+  useTabs: false,
+  arrowParens: "always",
+  trailingComma: "all",
 };
 
 export default config;
