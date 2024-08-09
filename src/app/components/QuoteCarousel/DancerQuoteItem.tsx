24b6805242bec4b9cc69c629623a4fd5f61e592c
bramirez96 <bran.ramirez.don@gmail.com>
2024-08-08 21:22:29 -0700

    Fix the quote text to prevent typographic orphans

--- src/app/components/QuoteCarousel/DancerQuoteItem.tsx
+++ src/app/components/QuoteCarousel/DancerQuoteItem.tsx
@@ -2,24 +2,33 @@ import type { ContentfulDancerQuote } from "~/types/Contentful";
 import { CarouselItem } from "~/components/ui/carousel";
 
 export interface DancerQuoteProps {
-  quoteEntry: ContentfulDancerQuote.Entry;
+    quoteEntry: ContentfulDancerQuote.Entry;
 }
 
 export function DancerQuoteItem({ quoteEntry }: DancerQuoteProps) {
-  return (
-    <CarouselItem>
-      <div className="flex h-full flex-col gap-2">
-        <div className="flex flex-grow flex-col justify-center">
-          <span className="text-center text-sm min-[400px]:text-base">
-            <span className="font-title">&#8220;</span>
-            {quoteEntry.fields.quote}
-            <span className="font-title">&#8221;</span>
-          </span>
-        </div>
-        <div className="px-8 text-right font-title text-2xl min-[650px]:text-3xl">
-          &#8211;&nbsp;{quoteEntry.fields.name}
-        </div>
-      </div>
-    </CarouselItem>
-  );
+    // Replace the final whitespace character in the text with a non-breaking space
+    const quoteText = quoteEntry.fields.quote
+        .split(/\s+/g)
+        .reduce((res, strPart, idx, arr) => {
+            if (!res) return strPart;
+            else if (idx === arr.length - 1) return `${res}\u00A0${strPart}`;
+            else return `${res} ${strPart}`;
+        }, "");
+
+    return (
+        <CarouselItem>
+            <div className="flex h-full flex-col gap-2">
+                <div className="flex flex-grow flex-col justify-center">
+                    <span className="text-center text-sm min-[400px]:text-base">
+                        <span className="font-title">&#8220;</span>
+                        {quoteText}
+                        <span className="font-title">&#8221;</span>
+                    </span>
+                </div>
+                <div className="px-8 text-right font-title text-2xl min-[650px]:text-3xl">
+                    &#8211;&nbsp;{quoteEntry.fields.name}
+                </div>
+            </div>
+        </CarouselItem>
+    );
 }
